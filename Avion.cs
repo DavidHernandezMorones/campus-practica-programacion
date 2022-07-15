ddc6937a47930f3a4c21b0b2f3b01d95e57f933d
DavidHernandezMorones <david.2.hernandez@atos.net>
2022-07-15 10:46:53 -0500

    Herencia

---
+++ Avion.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Practica {
+	internal sealed class Avion : Transporte {
+		private double _precio;
+		private string _tipoVuelo;
+
+		public Avion(DateTime salida, int pasajeros, string vuelo) : base(salida, pasajeros) {
+			if (IsChristmas())
+				_precio = 5000 * DescuentoPorHora() * 0.75;
+			else
+				_precio = 5000 * DescuentoPorHora();
+			
+			_tipoVuelo = vuelo;
+		}
+		
+		public double TotalPasajeros() {
+		    return _precio * numeroPasajeros;
+		}
+
+		public string Detalles () {
+			return $"El vuelo es {_tipoVuelo} y tiene un precio de {_precio}";
+		}
+	}
+}
---
+++ Transporte.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Practica {
+	internal abstract class Transporte {
+		protected DateTime salida;
+		protected int numeroPasajeros;
+
+		public Transporte(DateTime algunDia, int pasajeros) {
+			salida = algunDia;
+			numeroPasajeros = pasajeros;
+		}
+
+		protected double DescuentoPorHora() {
+			if (salida.Hour >= 15)
+				return 0.85;
+			if (salida.DayOfWeek == DayOfWeek.Saturday)
+				return 0.8;
+			return 1;
+		}
+
+		protected bool IsChristmas() {
+			return salida.Day == 25 && salida.Month == 12;
+		}
+	}
+}
